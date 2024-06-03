2f77b252d7fa935ced9df241614a49276d373098
Umang Chauhan <umang4191@outlook.com>
2024-06-03 21:55:28 +0200

    updated sqlalchemy

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 app = Flask(__name__)
 
+# Configure logging
+if not app.debug:
+    handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.ERROR)
+    app.logger.addHandler(handler)
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://admin123:#WRwinInferno2@flasktestdbserver.database.windows.net/testdb?driver=ODBC+Driver+17+for+SQL+Server'
@@ -29,14 +36,12 @@ def get_user(username):
 
 @app.route("/employees", methods=['GET'])
 def get_employees():
-    # Query all employees from the Employee table
-    employees = Employee.query.all()
-
-    # Convert the result to a list of dictionaries
-    employee_list = [{"ID": emp.ID, "FirstName": emp.FirstName, "LastName": emp.LastName} for emp in employees]
-
-    # Return the list as JSON
-    return jsonify(employee_list)
+    try:
+        employees = Employee.query.all()
+        return jsonify([{"ID": emp.ID, "FirstName": emp.FirstName, "LastName": emp.LastName} for emp in employees])
+    except Exception as e:
+        app.logger.error('Error fetching employees: %s', e)
+        return jsonify({"error": "Internal Server Error"}), 500
 
 if __name__ == "__main__":
     app.run()
